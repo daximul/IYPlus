602c58c8c7474839af74d28fd78f3d87d6651696
daximul <79027135+daximul@users.noreply.github.com>
2021-02-21 21:26:22 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,22 @@
-print("nothing yet")
+local Places = {
+	2768379856, -- SCP 3008
+}
+
+local function Import(Asset)
+	local Link = string.format("https://raw.githubusercontent.com/daximul/IYPlus/main/%s.lua", Asset)
+	local Response = game:HttpGetAsync(Link)
+	local Function = loadstring(Response)
+
+	local Success, Return = pcall(Function)
+	if (Success) then
+		return Return
+	else
+		warn("[IY+]: Failed to load Asset")
+	end
+end
+
+if table.find(Places, game.PlaceId) then
+	Import("Games/" .. game.PlaceId)
+else
+	Import("Scripts/IY_Config")
+end
